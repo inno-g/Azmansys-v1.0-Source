3e27907fda03dbdf59b05b4a9ec6a2a688aebadf
inno-g <paulinnocent5@gmail.com>
2020-07-17 03:04:02 +0100

    1.1

---
+++ CUESYSv.01/Form2.Designer.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CUESYSv._01
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.custContact = new System.Windows.Forms.TextBox();
+            this.Flyingfrom = new System.Windows.Forms.TextBox();
+            this.Destination = new System.Windows.Forms.TextBox();
+            this.Time = new System.Windows.Forms.TextBox();
+            this.Price = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.mySqlCommand1 = new MySql.Data.MySqlClient.MySqlCommand();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "custContact";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Flyingfrom";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Destination";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(85, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(86, 404);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Time";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(85, 433);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Price";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(85, 480);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Paid";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // custContact
+            // 
+            this.custContact.Location = new System.Drawing.Point(238, 7);
+            this.custContact.Name = "custContact";
+            this.custContact.Size = new System.Drawing.Size(100, 26);
+            this.custContact.TabIndex = 9;
+            // 
+            // Flyingfrom
+            // 
+            this.Flyingfrom.Location = new System.Drawing.Point(235, 54);
+            this.Flyingfrom.Name = "Flyingfrom";
+            this.Flyingfrom.Size = new System.Drawing.Size(100, 26);
+            this.Flyingfrom.TabIndex = 10;
+            // 
+            // Destination
+            // 
+            this.Destination.Location = new System.Drawing.Point(235, 94);
+            this.Destination.Name = "Destination";
+            this.Destination.Size = new System.Drawing.Size(100, 26);
+            this.Destination.TabIndex = 11;
+            // 
+            // Time
+            // 
+            this.Time.Location = new System.Drawing.Point(247, 398);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(100, 26);
+            this.Time.TabIndex = 13;
+            // 
+            // Price
+            // 
+            this.Price.Location = new System.Drawing.Point(247, 437);
+            this.Price.Name = "Price";
+            this.Price.Size = new System.Drawing.Size(100, 26);
+            this.Price.TabIndex = 14;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(235, 479);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(22, 21);
+            this.checkBox1.TabIndex = 20;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 518);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 42);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(174, 132);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 22;
+            // 
+            // mySqlCommand1
+            // 
+            this.mySqlCommand1.CacheAge = 0;
+            this.mySqlCommand1.Connection = null;
+            this.mySqlCommand1.EnableCaching = false;
+            this.mySqlCommand1.Transaction = null;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 581);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.Price);
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.Destination);
+            this.Controls.Add(this.Flyingfrom);
+            this.Controls.Add(this.custContact);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox custContact;
+        private System.Windows.Forms.TextBox Flyingfrom;
+        private System.Windows.Forms.TextBox Destination;
+        private System.Windows.Forms.TextBox Time;
+        private System.Windows.Forms.TextBox Price;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private MySql.Data.MySqlClient.MySqlCommand mySqlCommand1;
+    }
+}
---
+++ CUESYSv.01/Form2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CUESYSv._01
+{
+    public partial class Form2 : Form
+    {
+        dbConn mysqlConn = new dbConn();
+        public Form2()
+        {
+            InitializeComponent();
+            dbConfig();
+            mysqlConn.connect();
+        }
+        public bool dbConfig()
+        {
+            try
+            {
+                mysqlConn.varConfigServer = "airemionkp.cucstudents.org";
+                mysqlConn.varConfigDatabase = "airemionkp_";
+                mysqlConn.varConfigUser = "airemionkp";
+                mysqlConn.varConfigPass = "coventry01";
+                return true;
+            }
+            catch { return false; }
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+       
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mysqlConn.connOpen();
+            string date = monthCalendar1.SelectionRange.Start.ToString("yyyy-MM-dd");
+            string varPaid;
+            if (checkBox1.Checked == true) { varPaid = "Y"; }
+            else { varPaid = "N"; }
+            mysqlConn.insertBooking(custContact.Text, Flyingfrom.Text, Destination.Text, Time.Text, date, Price.Text, varPaid);
+            MessageBox.Show("Booking Finished Successfully");
+        }
+
+        
+
+        
+    }
+}
